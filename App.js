88626a8a1d1feadab73a0cedce717f47b07ac282
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2025-06-20 16:29:08 +0600

    Update App.js
    some update to JS

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-// const bulb = document.getElementById("bulb");
-// const btnOn = document.getElementById("btnOn");
-// const btnOff = document.getElementById("btnOff");
-// const btnToggle = document.getElementById("btnToggle");
 
-// btnOn.addEventListener("click", function () {
-//   bulb.classList.add("on")
-//   bulb.classList.remove("off")
-// });
+ const bulb = document.getElementById("bulb");
+ const btnOn = document.getElementById("btnOn");
+ const btnOff = document.getElementById("btnOff");
+ const btnToggle = document.getElementById("btnToggle");
 
-// btnOff.addEventListener("click", function () {
-//   bulb.classList.add("off")
-//   bulb.classList.remove("on")
-// });
+   btnOn.addEventListener("click", function () {
+   bulb.classList.add("on")
+   bulb.classList.remove("off")
+   });
 
-// btnToggle.addEventListener("click", function () {
-//   bulb.classList.toggle("on");
-//   bulb.classList.toggle("off");
-// });
+ btnOff.addEventListener("click", function () {
+  bulb.classList.add("off")
+  bulb.classList.remove("on")
+ });
+
+ btnToggle.addEventListener("click", function () {
+  bulb.classList.toggle("on");
+  bulb.classList.toggle("off");
+
+ });
