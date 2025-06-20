a5e9334b097d6d44fe24a1f70c03b23f999ace21
Selim-Ahmed93 <mdselimahmed6467@gmail.com>
2025-06-20 16:02:39 +0600

    DOM Learning Projects

---
+++ App.js
@@ -0,0 +1,19 @@
+// const bulb = document.getElementById("bulb");
+// const btnOn = document.getElementById("btnOn");
+// const btnOff = document.getElementById("btnOff");
+// const btnToggle = document.getElementById("btnToggle");
+
+// btnOn.addEventListener("click", function () {
+//   bulb.classList.add("on")
+//   bulb.classList.remove("off")
+// });
+
+// btnOff.addEventListener("click", function () {
+//   bulb.classList.add("off")
+//   bulb.classList.remove("on")
+// });
+
+// btnToggle.addEventListener("click", function () {
+//   bulb.classList.toggle("on");
+//   bulb.classList.toggle("off");
+// });
